b5fdd64d274081c558fd0527dfbc959a06177231
Toshiro Yagi <yagitoshiro@gmail.com>
2015-07-30 13:17:55 +0900

    basic phone

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,12 +4,17 @@ var authUrl = Alloy.Globals.authUrl;
 function connect(){
   twilio.connect({
     url: authUrl,
-    params: {}
+    params: {callTo: $.number.text}
   });
 }
 
 function disconnect(){
   twilio.disconnect();
+}
+
+function appendNumber(e){
+  var number = $.number.text;
+  $.number.text = number + e.source.title;
 }
 
 if(Ti.Platform.osname === 'android'){
@@ -20,8 +25,26 @@ if(Ti.Platform.osname === 'android'){
   pendingIntent = null;
 }
 
-$.connect.addEventListener('click', connect);
-$.disconnect.addEventListener('click', disconnect);
+$.clear.addEventListener('click', function(e){
+  $.number.text = '';
+});
+
+var status = true;
+$.mainButton.addEventListener('click', function(e){
+  if(status){
+    if($.number.text){
+      connect();
+      status = false;
+      $.mainButton.applyProperties({backgroundColor:'Red', title: '通話終了'});
+    }else{
+      alert("電話番号を入力してね");
+    }
+  }else{
+    disconnect();
+    status = true;
+    $.mainButton.applyProperties({backgroundColor:'#67BC45', title: '通話開始'});
+  }
+});
 
 $.index.addEventListener('open', function(e){
   twilio.addEventListener('loggedIn', function(e){
